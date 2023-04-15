4f47398b37684ac012c93dcbc172b23ec3275550
Aldin Febriansyah <aldin.20005@mhs.unesa.ac.id>
2023-04-15 14:51:01 +0700

    submit file function-1-cp.go

--- session-1/function-1-cp.go
+++ session-1/function-1-cp.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func luasLingkaran(jariJari int) float64 {
-	return 0 //hitung luas lingkaran
+	return math.Pi * float64(jariJari) * float64(jariJari) //hitung luas lingkaran
 }
 
 func kelilingLingkaran(jariJari int) float64 {
-	return 0 //hitung keliling lingkaran
+	return 2 * math.Pi * float64(jariJari) //hitung keliling lingkaran
 }
 
 func main() {
